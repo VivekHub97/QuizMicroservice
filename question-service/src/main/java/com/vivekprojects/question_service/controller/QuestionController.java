6a8e8c2e2842da77d27116467393226424154324
Vivek Agarwal <agarwalvivek.1997@gmail.com>
2024-11-18 06:40:24 +0100

    Preparing question service  (generate questions for Quiz, Post questions for Quiz, Calculate Score)

--- question-service/src/main/java/com/vivekprojects/question_service/controller/QuestionController.java
+++ question-service/src/main/java/com/vivekprojects/question_service/controller/QuestionController.java
@@ -10,9 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vivekprojects.question_service.model.Question;
+import com.vivekprojects.question_service.model.QuestionWrapper;
+import com.vivekprojects.question_service.model.Response;
 import com.vivekprojects.question_service.service.QuestionService;
 
 @RestController
@@ -43,5 +46,26 @@ public class QuestionController {
 	public ResponseEntity<String> addQuestion(@RequestBody Question question){		
 		return questionService.addQuestion(question);
 	}
+	
+	// generate
+	@GetMapping("generate")
+	public ResponseEntity<List<Integer>> getQuestionForQuiz(@RequestParam String categoryName, @RequestParam Integer numQuestions) {
+		
+		return questionService.getQuestionsForQuiz(categoryName, numQuestions);
+	}
+	
+	
+	// getQuestions (questionid)
+	@PostMapping("getQuestions")
+	public ResponseEntity<List<QuestionWrapper>> getQuestionFromId(@RequestBody List<Integer> questionIds) {
+	
+		return questionService.getQuestionsFromId(questionIds);
+	}
+	
+	// getScore
+	@PostMapping("getScore")
+	public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses) {
 
+		return questionService.getScore(responses);
+	}
 }
--- question-service/src/main/java/com/vivekprojects/question_service/service/QuestionService.java
+++ question-service/src/main/java/com/vivekprojects/question_service/service/QuestionService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import com.vivekprojects.question_service.dao.QuestionDao;
 import com.vivekprojects.question_service.model.Question;
+import com.vivekprojects.question_service.model.QuestionWrapper;
+import com.vivekprojects.question_service.model.Response;
 
 @Service
 public class QuestionService {
@@ -47,6 +49,44 @@ public class QuestionService {
 		// TODO Auto-generated method stub
 		questionDao.save(question);
 		return new ResponseEntity<>("Question Added", HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numQuestions) {
+		// TODO Auto-generated method stub
+		List<Integer> questions = questionDao.findRandomQuestionByCategory(categoryName, numQuestions);
+		
+		return new ResponseEntity<>(questions, HttpStatus.OK);
+	}
+
+	public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) {
+		// TODO Auto-generated method stub
+		
+		List<QuestionWrapper> wrappers = new ArrayList<>();
+		List<Question> questions = new ArrayList<>();
+		
+		for(Integer id : questionIds) {
+			questions.add(questionDao.findById(id).get());
+		}
+		
+		for(Question question : questions) {
+			QuestionWrapper wrapper = new QuestionWrapper(question.getId(), question.getQuestionTitle(), question.getOption1(), question.getOption2(), question.getOption3(), question.getOption4());
+			wrappers.add(wrapper);
+		}	
+		
+		return new ResponseEntity<>(wrappers, HttpStatus.OK);
+	}
+
+	public ResponseEntity<Integer> getScore(List<Response> responses) {
+		// TODO Auto-generated method stub
+		
+		int right = 0;
+		for(Response response : responses){
+			Question question = questionDao.findById(response.getId()).get();
+			if(response.getResponse().equals(question.getRightAnswer())) {
+				right++;
+			}	
+		}
+		return new ResponseEntity<>(right, HttpStatus.OK);	
 	}
 
 }
