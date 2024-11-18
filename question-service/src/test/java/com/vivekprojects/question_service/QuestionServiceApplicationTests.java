768f0aa8aed803bf9fa7f3c2fe3214f8131b94c3
Vivek Agarwal <agarwalvivek.1997@gmail.com>
2024-11-18 05:42:53 +0100

    question-service initialised for our microservices quiz boot application

---
+++ question-service/src/test/java/com/vivekprojects/question_service/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekprojects.question_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
